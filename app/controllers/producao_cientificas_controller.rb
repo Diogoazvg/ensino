b8c604f27d90674fb6db45029c3822fd8a9abe7a
Diogoazvg <diogo.azvg@gmail.com>
2014-10-12 04:45:22 -0300

    refatorando controller produção cientifica

--- app/controllers/producao_cientificas_controller.rb
+++ app/controllers/producao_cientificas_controller.rb
@@ -1,5 +1,5 @@
 class ProducaoCientificasController < InheritedResources::Base
 	def build_resource_params
-    	[params.fetch(:producao_cientificas, {}).permit(:artigo,:monografia)]
+    	[params.fetch(:producao_cientifica, {}).permit(:artigo,:monografia)]
     end
 end
--- app/models/producao_cientifica.rb
+++ app/models/producao_cientifica.rb
@@ -1,3 +1,3 @@
 class ProducaoCientifica < ActiveRecord::Base
-	has_and_belongs_to_many :prodfessores
+	has_and_belongs_to_many :professores
 end
