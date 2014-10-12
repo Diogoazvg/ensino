090ce004bed5d8cbc99b20384ab2bc3b7359ebd9
Diogoazvg <diogo.azvg@gmail.com>
2014-10-12 04:06:29 -0300

    refatorando model produção cientifica

--- spec/models/producao_cientifica_spec.rb
+++ spec/models/producao_cientifica_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ProducaoCientifica, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "can be instantiated" do
+  	producaoCientifica = ProducaoCientifica.new
+  	expect(producaoCientifica).to be_a ProducaoCientifica
+  end
 end
