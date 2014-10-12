67e06d41ce575904a6104c319563d3e4c100f098
Diogoazvg <diogo.azvg@gmail.com>
2014-10-12 04:00:55 -0300

    refatorando model titulação

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Professor, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "can be instantiated" do
+  	titulacao = Titulacao.new
+  	expect(titulacao).to be_a Titulacao
+  end	
 end
